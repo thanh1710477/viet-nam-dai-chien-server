51dec61d365e14204604e7edd54e66d18369fed4
Vo Duc Thanh <vdthanh1998@gmail.com>
2026-01-21 20:58:27 +0700

    Fix email timeout using port 587

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,9 @@ const app = express();
 
 // Mail Transporter Setup
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
